f5e0b6a184d4c3525bd4ef50ce09c571e8514487
AdmiringWorm <kim.nordmo@gmail.com>
2021-11-06 23:43:27 +0000

    chore: initial commit

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+// Copyright (c) 2021 Kim J. Nordmo and WormieCorp.
+// Licensed under the Apache 2.0 license. See LICENSE.txt file in the project
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
